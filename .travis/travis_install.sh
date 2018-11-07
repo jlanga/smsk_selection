3a66fbff3f7e34f0b1665f4fd1de886c6ac758e5
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-11-07 17:23:42 +0100

    Linting

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 export PATH="$HOME"/miniconda3_"$TRAVIS_OS_NAME"/bin:"$PATH"
-echo $PATH
+echo "$PATH"
 
 conda config --add channels defaults
 conda config --add channels conda-forge
