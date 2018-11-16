2c513ee5d4c957dc4a69ee041e1674a367efc550
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-11-16 16:50:23 +0100

    Possible acceleration in conda?

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -4,9 +4,13 @@ set -euo pipefail
 export PATH="$HOME"/miniconda3_"$TRAVIS_OS_NAME"/bin:"$PATH"
 echo "$PATH"
 
+echo "local_repodata_ttl: 1800" >> ~/.condarc
+
 conda config --add channels defaults
 conda config --add channels conda-forge
 conda config --add channels bioconda
 
 conda install --yes --channel bioconda snakemake
 conda clean --all --yes
+
+snakemake --use-conda
