a1b7b9ba4ae3b022d5204e4a5005831513171d3e
Jorge Langa <jorge.langa.arranz@gmail.com>
2019-12-16 17:55:34 +0100

    Faster tree_to_fasta

--- src/tree_to_fasta.py
+++ src/tree_to_fasta.py
@@ -14,7 +14,7 @@ from Bio import Phylo
 def get_leafs(filein):
     """Just get the leaf names of a tree"""
     return [
-        leaf_name
+        leaf_name.name
         for leaf_name in Phylo.read(filein, format='newick').get_terminals()
     ]
 
@@ -25,6 +25,31 @@ def fasta_to_dict(filein):
             identifier.split()[0]: sequence
             for identifier, sequence in SimpleFastaParser(handle)
         }
+
+
+def process(fasta_dict, in_dir, in_ext, out_dir, out_ext):
+    """Compose fasta clusters given the files in in_dir"""
+    in_files = [
+        in_dir + in_file
+        for in_file in os.listdir(in_dir)
+        if in_file.endswith(in_ext)
+    ]
+
+    for in_file in in_files:
+        prefix = in_file.split("/")[-1].split(in_ext)[0]
+        out_file = out_dir + prefix + out_ext
+        sys.stderr.write(in_file + " => " + out_file + "\n")
+        seq_names = get_leafs(in_file)
+        with open(out_file, "w") as fout:
+            for seq_name in seq_names:
+                fout.write(
+                    ">{seq_name}\n{seq}\n".format(
+                        seq_name=seq_name,
+                        seq=fasta_dict[seq_name]
+                    )
+                )
+
+
 
 if __name__ == '__main__':
 
@@ -49,22 +74,5 @@ if __name__ == '__main__':
 
     FASTA_DICT = fasta_to_dict(FASTA)
 
-    IN_FILES = [
-        in_file
-        for in_file in os.listdir(IN_DIR)
-        if in_file.endswith(IN_EXT)
-    ]
-
-    for in_file in IN_FILES:
-        prefix = in_file.split("/")[-1].split(IN_EXT)[0]
-        out_file = OUT_DIR + prefix + OUT_EXT
-
-        seq_names = get_leafs(in_file)
-        with open(out_file, "w") as fout:
-            for seq_name in seq_names:
-                fout.write(
-                    ">{seq_name}\n{seq}".format(
-                        seq_name=seq_name,
-                        seq=FASTA_DICT[seq_name]
-                    )
-                )
+    process(FASTA_DICT, IN_DIR, IN_EXT, OUT_DIR, OUT_EXT)
+    
