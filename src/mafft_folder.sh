e18bf5da6aba68f878065cbf14d734d1d7964851
Jorge Langa <jorge.langa.arranz@gmail.com>
2019-12-17 11:36:02 +0100

    Second round of mafft for dead processes

--- src/mafft_folder.sh
+++ src/mafft_folder.sh
@@ -11,25 +11,20 @@ threads=$5
 
 mkdir -p "$out_dir"
 
-#for filein in "$in_dir"/*."$in_ext" ; do
-
-#    cluster_id=$(basename -- "$filein" ."$in_ext")
-
-#    fileout="$out_dir/${cluster_id}.$out_ext"
-
-#    if [[ -f "$fileout" ]] ; then
-
-#        >&2 echo "$fileout exits. Skipping."
-
-#    else
-
-#        mafft --amino --auto --maxiterate 1000 --thread "$threads" "$filein" > "$fileout"
-
-#    fi
-
-#done
-
 parallel \
     --jobs "$threads" \
     mafft --amino --genafpair --maxiterate 1000 {} ">" "$out_dir"/{/.}."$out_ext" \
 ::: "$in_dir"/*."$in_ext"
+
+# Second round of dead processes, this time one by one multithreaded
+find "$out_dir" -name "*.$out_ext" -size 0 \
+| parallel \
+    --pipe \
+    --jobs 1 \
+    mafft \
+        --amino \
+        --genafpair \
+        --maxiterate 1000 \
+        --thread $threads \
+        "$in_dir"/{/.}."$in_ext" \
+    ">" "$out_dir"/{/.}."$out_ext" 
