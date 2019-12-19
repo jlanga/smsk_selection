40ee642161e08d2d491d7c4a98289321f4a1f311
Jorge Langa <jorge.langa.arranz@gmail.com>
2019-12-19 17:40:27 +0100

    parallel monotrheaded raxml-ng

--- src/homologs/raxmlng_folder.sh
+++ src/homologs/raxmlng_folder.sh
@@ -1,47 +1,96 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+#  bash src/homologs/raxmlng_folder.sh in_dir in_ext out_dir out_ext threads
 
 in_dir=$1
 in_ext=$2
 out_dir=$3
-out_ext=$4
-threads=$5
+threads=$4
 
 mkdir -p "$out_dir"
 
-for filein in "$in_dir"/*."$in_ext" ; do
 
-    echo "$filein"
-
-    cluster_id=$(basename -- "$filein" ."$in_ext")
-
-    prefix="$out_dir"/"$cluster_id"
-    fileout="$prefix"."$out_ext"
-
-    if [[ -f "$fileout" ]] ; then
-
-        >&2 echo "$fileout exits. Skipping."
+raxml_wrapper() {
     
-    elif [[ $(grep -c ^">" "$filein") -ge 4 ]] ; then
 
-        raxml-ng --check --msa "$filein" --model WAG
+    file_in=$1
+    in_ext=$2
+    out_dir=$3
+    
+    in_dir=$(dirname "$file_in")
+    cluster_id=$(basename -- "$file_in" ."$in_ext")
+
+    prefix_out="$out_dir/$cluster_id"
+    file_out="$out_dir/$cluster_id".raxml.bestTree
+
+
+    if [[ -f "$file_out" ]] ; then
+
+        >&2 echo "$file_out exits. Skipping."
+    
+    elif [[ $(grep -c ^">" "$file_in") -ge 4 ]] ; then
+
+        raxml-ng --check --msa "$file_in" --model WAG
 
         raxml-ng \
-            --msa "$filein" \
+            --msa "$file_in" \
             --msa-format FASTA \
             --data-type AA \
-            --prefix "$prefix" \
+            --prefix "$prefix_out" \
             --model WAG  \
             --seed 12345 \
-            --threads "$threads" \
+            --threads 1 \
         1>&2
 
     else 
 
-        >&2 echo "$filein has less than 4 sequences. Skipping."
+        >&2 echo "$file_in has less than 4 sequences. Skipping."
 
     fi
 
+}
 
-done
+
+export -f raxml_wrapper
+
+parallel \
+    --jobs "$threads" \
+    raxml_wrapper {} "$in_ext" "$out_dir" \
+::: "$in_dir"/*."$in_ext"
+
+# for filein in "$in_dir"/*."$in_ext" ; do
+
+#     echo "$filein"
+
+#     cluster_id=$(basename -- "$filein" ."$in_ext")
+
+#     prefix="$out_dir"/"$cluster_id"
+#     fileout="$prefix"."$out_ext"
+
+#     if [[ -f "$fileout" ]] ; then
+
+#         >&2 echo "$fileout exits. Skipping."
+    
+#     elif [[ $(grep -c ^">" "$filein") -ge 4 ]] ; then
+
+#         raxml-ng --check --msa "$filein" --model WAG
+
+#         raxml-ng \
+#             --msa "$filein" \
+#             --msa-format FASTA \
+#             --data-type AA \
+#             --prefix "$prefix" \
+#             --model WAG  \
+#             --seed 12345 \
+#             --threads "$threads" \
+#         1>&2
+
+#     else 
+
+#         >&2 echo "$filein has less than 4 sequences. Skipping."
+
+#     fi
+
+
+# done
