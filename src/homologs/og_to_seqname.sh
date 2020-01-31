c8ac589340c7194b5b0e1dd429c71652e933342c
Jorge Langa <jorge.langa.arranz@gmail.com>
2020-01-31 17:39:07 +0100

    Simplify generic + og_to_seqname.sh

---
+++ src/homologs/og_to_seqname.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+process_orthogroup(){
+
+    fullname=$1
+    filename=$(basename -- "$fullname")
+    filename="${filename%.*}"
+
+    for seqname in $(grep ^">" "$fullname" | tr -d ">"); do
+        echo -e "$filename\t$seqname"
+    done
+
+}
+
+export -f process_orthogroup    
+
+find "$1" -name "*.fa" -type f \
+| sort --version-sort \
+| parallel --keep-order \
+    process_orthogroup 
